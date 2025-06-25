2515714782b4bafb0b17148d39115d28362a081a
glaylson87 <etelvino88@gmail.com>
2025-06-25 13:59:06 -0300

    Sorteador de numero

---
+++ scrips.js
@@ -0,0 +1,13 @@
+function sortearNumero(){
+    
+    const min = Math.ceil(document.querySelector(".min").value)
+
+    const max = Math.floor(document.querySelector(".max").value)
+
+ 
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  alert (result)
+}
+
+
