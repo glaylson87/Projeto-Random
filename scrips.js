b4dd342a0211e6fa2908b8371f5f98c966b873fb
glaylson87 <etelvino88@gmail.com>
2025-06-25 18:14:50 -0300

    alteração no js

--- scrips.js
+++ scrips.js
@@ -1,13 +1,22 @@
-function sortearNumero(){
-    
-    const min = Math.ceil(document.querySelector(".min").value)
+function sortearNumero() {
 
-    const max = Math.floor(document.querySelector(".max").value)
+  const min = Math.ceil(document.querySelector(".min").value)
 
- 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  const max = Math.floor(document.querySelector(".max").value)
 
-  alert (result)
+  if (max <= min) {
+    alert("porfavor coloque um numero Maior que o minimo")
+  }
+  else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+  }
+
 }
+
+
+
+
 
 
